8d84abba5c35b17590d4b86f123e12b701c72463
Matthew Gasiorek <matthew.gasiorek.coding@gmail.com>
2025-05-09 19:13:29 -0400

    Trying out C

---
+++ c/hello_world/hello_world.c
@@ -0,0 +1,17 @@
+/*
+ * Project Name: Haversine Formula Visualizer
+ * Author: Matthew Gasiorek
+ * Created On: 2025-05-09
+ * Last Modified: 2025-05-09
+ * Description: I want this to be a fun, introductory project where I can learn the basics of C and a cool math concept that I learned.
+ * The Haversine Formula determines the great-cicle distance between two points on a sphere given their longitudes and latitudes.asm
+ * This is especially important in navigation, where one might want to know the distance between two points on Earth.
+ * Version: 1.0
+ * Contact: matthew.gasiorek.coding@gmail.com
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
